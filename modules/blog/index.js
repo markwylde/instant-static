888ab0b0fc20b34431e032e49fdcbc4ea3fad178
Mark Wylde <mark.wylde@verl.com>
2016-09-14 22:28:43 +0100

    put url path in blog module

--- modules/blog/index.js
+++ modules/blog/index.js
@@ -1,16 +1,19 @@
 const fs = require('fs-promise')
 const yaml = require('js-yaml')
 const marked = require('marked')
+const moment = require('moment')
 
 module.exports = {
-  fetchPosts: async function() {
-    const files = await fs.readdir('./content/posts/')
+  fetchPosts: async function(settings) {
+    const files = await fs.readdir('./content/blog/')
     const posts = await Promise.all(
       files.map(async file =>
-        fs.readFile('./content/posts/' + file, 'utf8')
+        fs.readFile('./content/blog/' + file, 'utf8')
           .then(post => `id: ${file.slice(0,-4)}\n${post}`)
           .then(post => yaml.load(post))
           .then(post => {
+            post.date = new Date(post.date)
+            post.path = `${settings.url}${moment(post.date).format('YYYY/MM/DD')}/${post.id}`
             if (post.type === 'markdown') {
               post.content = marked(post.content)
               return post
