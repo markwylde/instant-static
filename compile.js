86cb890c80541a13c918d0db5329856e5e18a62b
Mark Wylde <mark.wylde@verl.com>
2016-09-17 12:58:16 +0100

    extract render functions into modules for modularity

--- compile.js
+++ compile.js
@@ -1,66 +1,31 @@
-const ejs = require('ejs')
 const fs = require('fs-extra-promise')
 const rimraf = require('rimraf-promise')
 const blog = require('./modules/blog')
-const ncp = require('ncp').ncp
-const moment = require('moment')
-const mkdirp = require('mkdirp-promise')
+const shop = require('./modules/shop')
+const home = require('./modules/home')
 const yaml = require('js-yaml')
-
-let settings, templates = {}
-
-async function render(file, scope) {
-  if (!templates[file]) {
-    templates[file] = await fs.readFileAsync(file, 'utf8')
-  }
-  const template = templates[file]
-  return ejs.render(template, Object.assign(scope, {moment, settings}))
-}
+const groupBy = require('./utils/groupBy').default
 
 async function main() {
 
   await rimraf('./dist')
   await fs.mkdir('./dist')
 
-  settings = await fs.readFileAsync('./content/settings.yml', 'utf8')
-  settings = yaml.load(settings)
-
   await fs.copySync('./theme/css', './dist/css')
+  await fs.copySync('./content/images', './dist/images')
 
+  const settings = yaml.load(await fs.readFileAsync('./content/settings.yml', 'utf8'))
   const posts = await blog.fetchPosts(settings)
-  
+  const products = await shop.fetchProducts(settings)
+  const categories = groupBy('category', products)
+
   await Promise.all([
-    renderPosts(posts),
-    renderHomePage(posts)
+    posts.map(blog.renderPost),
+    categories.map(shop.renderCategory),
+    products.map(shop.renderProduct),
+
+    home.render({posts, products})
   ])
-
-  // -----------------------------------------------
-
-  async function renderPosts(posts) {
-    return Promise.all(posts.map(renderPost))
-  }
-
-  async function renderPost(post) {
-      const compiled = await render('./theme/layouts/main.ejs', {
-        title: post.title,
-        content: await render('./theme/blog/post.ejs', { post }),
-      })
-      const path = `./dist/${moment(post.date).format('YYYY/MM/DD')}/${post.id}`
-      await mkdirp(path)
-      return fs.writeFile(`${path}/index.html`, compiled)
-  }
-
-
-  async function renderHomePage(posts) {
-    const compiled = await render('./theme/layouts/main.ejs', {
-      title: "Welcome",
-      content: await render('./theme/blog/home.ejs', { posts })
-    })
-
-    fs.writeFile('./dist/index.html', compiled)
-  }
-
-
 }
 
-main().catch(console.log)
+main().catch(console.error)
