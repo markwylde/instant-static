24dd795b372f527d55ed0e151684907bb8dee134
Mark Wylde <mark.wylde@verl.com>
2016-09-17 12:58:49 +0100

    add linting definitations for cleaner code

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": [
+            "error", { allow: ["error"] }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
